1e94606ad4a05a8cf482125df3da82512b59090b
Victor <ban.victor728@gmail.com>
2024-07-25 12:47:32 -0400

    fixing commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
-import Nav from './components/Nav.jsx'
+
 import Booklist from './components/Booklist.jsx'
 import BookDetails from './components/BookDetails.jsx'
 
 
-const App = () => {
 
+const App = () => {
 
   return (
     <>
       <nav></nav>
-      <h1>Main Page</h1>
-      <main>
-        <Booklist />
 
-
-      </main>
-
+    <Routes>
+      <Route path='/' element={<Booklist />}/>
+      <Route path='/books/:id' element={<BookDetails />} />
+    </Routes>
 
     </>
   )
